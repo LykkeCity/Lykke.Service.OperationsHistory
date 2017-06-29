afa20de14f173924803f97c8109c9ac576be767a
atarutin <tarutin_andrew@mail.ru>
2017-06-29 21:41:44 +0300

    updating components

---
+++ tests/Lykke.Services.OperationsHistory.Job.Tests/HistoryJobTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lykke.Services.OperationsHistory.Job.Tests
+{
+    [TestClass]
+    public class HistoryJobTests
+    {
+        public static IServiceProvider Services { get; set; }
+        //public static ILog Logger = Services.GetService<ILog>();
+        [AssemblyInitialize]
+        public static void Init(TestContext context)
+        {
+            //var cBuilder = new ContainerBuilder();
+        }
+        [TestMethod]
+        public void TestValidate()
+        {
+        }
+    }
+}
