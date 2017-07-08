83dea002d9410330d651c9825340c88735c30a48
atarutin <tarutin_andrew@mail.ru>
2017-07-09 00:16:54 +0300

    changed service project to be created from template

---
+++ src/Lykke.Service.OperationsHistory/Modules/ServiceModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Common.Log;
+using Lykke.Service.OperationsHistory.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lykke.Service.OperationsHistory.Modules
+{
+    public class ServiceModule : Module
+    {
+        private readonly OperationsHistorySettings _settings;
+        private readonly ILog _log;
+        // NOTE: you can remove it if you don't need to use IServiceCollection extensions to register service specific dependencies
+        private readonly IServiceCollection _services;
+
+        public ServiceModule(OperationsHistorySettings settings, ILog log)
+        {
+            _settings = settings;
+            _log = log;
+
+            _services = new ServiceCollection();
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(_settings)
+                .SingleInstance();
+
+            builder.RegisterInstance(_log)
+                .As<ILog>()
+                .SingleInstance();
+
+            // TODO: Add your dependencies here
+
+            builder.Populate(_services);
+        }
+    }
+}
