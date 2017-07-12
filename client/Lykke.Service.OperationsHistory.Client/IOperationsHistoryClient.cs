1f0385405e92997532706cd4eb873987a46f7597
atarutin <tarutin_andrew@mail.ru>
2017-07-12 17:47:12 +0300

    added Operation history client interface and stubbed implementation

--- client/Lykke.Service.OperationsHistory.Client/IOperationsHistoryClient.cs
+++ client/Lykke.Service.OperationsHistory.Client/IOperationsHistoryClient.cs
@@ -1,7 +1,22 @@
 
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Service.OperationsHistory.AutorestClient.Models;
+
 namespace Lykke.Service.OperationsHistory.Client
 {
     public interface IOperationsHistoryClient
     {
+        Task<IList<HistoryEntryResponse>> AllAsync(string clientId, int top, int skip);
+        Task<IList<HistoryEntryResponse>> AllAsync(string clientId, int page);
+        Task<IList<HistoryEntryResponse>> ByOperationAsync(string clientId, string operationType, int top, int skip);
+        Task<IList<HistoryEntryResponse>> ByOperationAsync(string clientId, string operationType, int page);
+        Task<IList<HistoryEntryResponse>> ByOperationAndAssetAsync(string clientId, string operationType,
+            string assetId, int top, int skip);
+        Task<IList<HistoryEntryResponse>> ByOperationAndAssetAsync(string clientId, string operationType,
+            string assetId, int page);
+        Task<IList<HistoryEntryResponse>> ByAssetAsync(string clientId, string assetId, int top, int skip);
+        Task<IList<HistoryEntryResponse>> ByAssetAsync(string clientId, string assetId, int page);
     }
 }
--- client/Lykke.Service.OperationsHistory.Client/OperationsHistoryClient.cs
+++ client/Lykke.Service.OperationsHistory.Client/OperationsHistoryClient.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Common.Log;
+using Lykke.Service.OperationsHistory.AutorestClient;
+using Lykke.Service.OperationsHistory.AutorestClient.Models;
 
 namespace Lykke.Service.OperationsHistory.Client
 {
     public class OperationsHistoryClient : IOperationsHistoryClient, IDisposable
     {
         private readonly ILog _log;
+        private readonly IOperationsHistoryAPI _apiClient;
 
         public OperationsHistoryClient(string serviceUrl, ILog log)
         {
             _log = log;
+            _apiClient = new OperationsHistoryAPI(new Uri(serviceUrl));
         }
 
         public void Dispose()
@@ -18,6 +24,46 @@ namespace Lykke.Service.OperationsHistory.Client
             //    return;
             //_service.Dispose();
             //_service = null;
+        }
+
+        public Task<IList<HistoryEntryResponse>> AllAsync(string clientId, int top, int skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> AllAsync(string clientId, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByOperationAsync(string clientId, string operationType, int top, int skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByOperationAsync(string clientId, string operationType, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByOperationAndAssetAsync(string clientId, string operationType, string assetId, int top, int skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByOperationAndAssetAsync(string clientId, string operationType, string assetId, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByAssetAsync(string clientId, string assetId, int top, int skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<HistoryEntryResponse>> ByAssetAsync(string clientId, string assetId, int page)
+        {
+            throw new NotImplementedException();
         }
     }
 }
