1919220db488063c1a25580bed2e2088d091daad
atarutin <tarutin_andrew@mail.ru>
2017-07-24 14:50:06 +0200

    moved abstractions from main project to nuget package

---
+++ client/Lykke.Service.OperationsHistory.HistoryWriter/Abstractions/IHistoryMapper.cs
@@ -0,0 +1,9 @@
+﻿using Lykke.Service.OperationsHistory.HistoryWriter.Model;
+
+namespace Lykke.Service.OperationsHistory.HistoryWriter.Abstractions
+{
+    public interface IHistoryMapper<in T>
+    {
+        HistoryEntry MapFrom(T source);
+    }
+}
---
+++ client/Lykke.Service.OperationsHistory.HistoryWriter/Implementation/RepositoryWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lykke.Service.OperationsHistory.HistoryWriter.Abstractions;
+using Lykke.Service.OperationsHistory.HistoryWriter.Model;
+
+namespace Lykke.Service.OperationsHistory.HistoryWriter.Implementation
+{
+    public abstract class RepositoryWriter<TRepo, TRepoItem> : IHistoryMapper<TRepoItem>
+    {
+        protected readonly TRepo Repo;
+        protected readonly IHistoryWriter HistoryWriter;
+
+        protected RepositoryWriter(TRepo repo, IHistoryWriter historyWriter)
+        {
+            Repo = repo;
+            HistoryWriter = historyWriter;
+        }
+
+        public HistoryEntry MapFrom(TRepoItem source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return DoMapFrom(source);
+        }
+
+        protected virtual HistoryEntry DoMapFrom(TRepoItem source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
